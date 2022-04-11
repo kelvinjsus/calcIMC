0fc5519738ef02c773f3cf8f8c6761385239834b
kelvinjsus <93455951+kelvinjsus@users.noreply.github.com>
2022-04-11 01:50:04 -0300

    Add files via upload

--- style.js
+++ style.js
@@ -6,7 +6,6 @@ export default StyleSheet.create({
     flex: 1,
     backgroundColor: '#f3f3f3',
     alignItems: 'center',
-    // justifyContent: 'center'
   },
   title: {
     paddingBottom: 30,
